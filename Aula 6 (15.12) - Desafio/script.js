88bbc5d9794af318600f0d06f4badfbf73f73a05
andrevpc <andrevictor06@gmail.com>
2022-12-17 11:34:21 -0300

    port

---
+++ Aula 6 (15.12) - Desafio/script.js
@@ -0,0 +1,7 @@
+function openDescription(){
+    document.getElementById("popup").style.display = "block";
+}
+function closeDescription(){
+    document.getElementById("popup").style.display = "none";
+}
+
