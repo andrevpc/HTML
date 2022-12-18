9ee5198e494a7a52928ac67d6e0442b8e3a1b866
andrevpc <andrevictor06@gmail.com>
2022-12-18 18:26:10 -0300

    Falta executaveis

--- Aula 6 (15.12) - Desafio/script.js
+++ Aula 6 (15.12) - Desafio/script.js
@@ -5,3 +5,23 @@ function closeDescription(){
     document.getElementById("popup").style.display = "none";
 }
 
+function openDescription1(){
+    document.getElementById("popup1").style.display = "block";
+}
+function closeDescription1(){
+    document.getElementById("popup1").style.display = "none";
+}
+
+function openDescription2(){
+    document.getElementById("popup2").style.display = "block";
+}
+function closeDescription2(){
+    document.getElementById("popup2").style.display = "none";
+}
+
+function openDescription3(){
+    document.getElementById("popup3").style.display = "block";
+}
+function closeDescription3(){
+    document.getElementById("popup3").style.display = "none";
+}
